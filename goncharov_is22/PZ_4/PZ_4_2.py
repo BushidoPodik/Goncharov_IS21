44ff689432d3da726f6c133be2e6d00b72f0175b
BushidoPodik <113247983+BushidoPodik@users.noreply.github.com>
2022-12-14 10:22:24 +0300

    Update PZ_4_2.py

--- goncharov_is22/PZ_4/PZ_4_2.py
+++ goncharov_is22/PZ_4/PZ_4_2.py
@@ -1,8 +1,8 @@
-n = int(input('ведите число:'))
-c = 0
-s = 0
-while n > 0:
-    c += 1
-    s += n % 10
-    n //= 10
-print(c, s)
+import random
+N = random.randrange(1, 10000000)
+print("N = ", N)
+q = N
+while q >= 1:
+    r = q % 10
+    print(r, end="; ")
+    q = int(q/10)
